85021a8b057e6cfd5d85692303b1f6b38ff3d652
omwami <ianalpha@protonmail.com>
2024-03-05 17:31:55 +0300

    read file synchronously with nodejs

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,45 +1,38 @@
 const fs = require('fs');
-const readline = require('readline');
 
-const countStudents = (path) => {
-  const stream = fs.createReadStream(path);
-
-  stream.on('error', (error) => {
-    throw new Error('Cannot load the database');
-  });
-
-  const rl = readline.createInterface({ input: stream });
-  const studentsPerField = {};
-  let count = 0;
-  let isFirstLine = true;
-
-  rl.on('line', (row) => {
-    if (isFirstLine) {
-      // avoid reading first line of csv file (fields)
-      isFirstLine = false;
-      return;
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  try {
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
     }
-
-    count++;
-    const data = row.split(',');
-    if (data.length === 0) {
-      count--; // empty line or improper db
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
     }
-    if (studentsPerField.hasOwnProperty(data[3])) {
-      studentsPerField[data[3]].push(` ${data[0]}`);
-    } else {
-      studentsPerField[data[3]] = [];
-      studentsPerField[data[3]].push(` ${data[0]}`);
-    }
-  });
-
-  rl.on('close', () => {
-    console.log(`Number of students: ${count}`);
-    for (const [key, value] of Object.entries(studentsPerField)) {
-      field_l = value.length;
-      console.log(`Number of students in ${key}: ${field_l}. List:${value}`);
-    }
-  });
-};
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
 
 module.exports = countStudents;
