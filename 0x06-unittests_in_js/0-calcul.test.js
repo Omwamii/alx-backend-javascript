63d38ff3be4a9600897d21c2b1a0b4086ad0645a
omwami <ianalpha@protonmail.com>
2024-05-12 11:23:31 +0300

    test for calculate fn

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,6 +11,10 @@ describe('calculateNumber test', () => {
         assert.strictEqual(fn(1.2, 3.7), 5)
     })
 
+    it('Test first number rounded', () => {
+        assert.strictEqual(fn(1.5, 3.2), 5)
+    })
+    
     it('Test integer and decimal', () => {
         assert.strictEqual(fn(1, 3.7), 5)
     })
